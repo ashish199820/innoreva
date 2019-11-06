a11cc90f1a5c1a9e82dab0964ea2680e842c4f3c
ashish <ashish.aryan099@gmail.com>
2019-11-06 14:52:37 +0530

    adding post route

--- app.js
+++ app.js
@@ -1,17 +1,20 @@
 var express =require("express");
 var dotenv = require("dotenv");
 var app =express();
+var bodyParser=require("body-parser")
 //var request= require("request");
 dotenv.config();
 var ejs= require("ejs");
 var galleryRoute = require('./router/galleryRoute');
+var postRoute = require('./router/postRoute')
 
 //app.use("/gallery",galleryRoute);
 app.set("view engine","ejs");   ///set template engine to ejs
-//app.use("bodyParser.urlencoded{extended:true");
+app.use("bodyParser.urlencoded{extended:true");
 //////it will have two routes 1. search 2. result
 app.use("/public",express.static(__dirname+'/public'));
 app.use('/gallery',galleryRoute);
+app.use('/post',post);
 
 
 app.get("/",function(req,res)
