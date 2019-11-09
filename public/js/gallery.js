141574248196c1603bbe22e7acb54be77e50c263
ashish <ashish.aryan099@gmail.com>
2019-11-09 23:08:19 +0530

    modified ..

--- public/js/gallery.js
+++ public/js/gallery.js
@@ -5,10 +5,10 @@ $(function() {
     $(".gbutton").click(function(){
     selectedClass = $(this).attr("data-rel");
     //$("#gallery").fadeTo(100, 0.1);
-    $("#gallerySLide span").not("."+selectedClass).fadeOut().css("display:inline");
+    $("#gallerySLide div").not("."+selectedClass).fadeOut().css("display:inline");
     setTimeout(function() {
-    $("."+selectedClass).fadeIn().css("display:inline");
-    $("#gallerySLide").fadeTo(300, 1).css("display:inline");
+    $("."+selectedClass).fadeIn();
+    $("#gallerySLide").fadeTo(300, 1);
     }, 300);
     });
     });
