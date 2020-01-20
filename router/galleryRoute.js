68b9be48340be45ab488950a6560616f10508d94
ashish <ashish.aryan099@gmail.com>
2020-01-20 19:27:00 +0530

    minor changes

--- router/galleryRoute.js
+++ router/galleryRoute.js
@@ -1,7 +1,7 @@
 var express = require('express');
 var router   =express.Router();
 var {Gallery} = require("../model/gallery");
-var {Tag} = require("../model/tag");
+var Tag = require("../model/tag");
 
 
 router.get('/',async function(req,res){
@@ -10,7 +10,7 @@ router.get('/',async function(req,res){
     var img= await Gallery.find({});
         res.render('gallery.ejs',{tags:tags,images:img});
     }catch(err){
-        res.send(err.details[0].message);
+        res.send(err);
     }
 })
 
